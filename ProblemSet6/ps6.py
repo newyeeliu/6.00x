1f7bad1f59d27b1b228a0b3736f0a79ac098636f
David Johnston <david.johnston@beatsystems.com>
2012-11-13 15:13:03 +0000

    Add Not, And and Or Triggers

--- ProblemSet6/ps6.py
+++ ProblemSet6/ps6.py
@@ -108,6 +108,9 @@ class TitleTrigger(WordTrigger):
         
 class SubjectTrigger(WordTrigger):
         
+    def evaluate(self, story):
+        return self.isWordIn(story.getSubject())
+        
 class SummaryTrigger(WordTrigger):
         
     def evaluate(self, story):
@@ -117,8 +120,32 @@ class SummaryTrigger(WordTrigger):
 # Composite Triggers
 # Problems 6-8
 
-# TODO: NotTrigger
-# TODO: AndTrigger
+class NotTrigger(Trigger):
+    
+    def __init__(self, trigger):
+        self.trigger = trigger
+        
+    def evaluate(self, story):
+        return not self.trigger.evaluate(story)
+
+class AndTrigger(Trigger):
+    
+    def __init__(self, trigger1, trigger2):
+        self.trigger1 = trigger1
+        self.trigger2 = trigger2
+        
+    def evaluate(self, story):
+        return self.trigger1.evaluate(story) and self.trigger2.evaluate(story)
+
+class OrTrigger(Trigger):
+    
+    def __init__(self, trigger1, trigger2):
+        self.trigger1 = trigger1
+        self.trigger2 = trigger2
+        
+    def evaluate(self, story):
+        return self.trigger1.evaluate(story) or self.trigger2.evaluate(story)
+
 # TODO: OrTrigger
 
 
